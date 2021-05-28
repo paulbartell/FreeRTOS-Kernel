841e9a21dac42713bed64c6e57da05710c5d907a
paulbartell <paulbartell@users.noreply.github.com>
2021-05-28 09:56:36 +0000

    [AUTO][RELEASE]: Bump task.h version macros to "V10.4.487#"

--- include/task.h
+++ include/task.h
@@ -46,10 +46,10 @@
 * MACROS AND DEFINITIONS
 *----------------------------------------------------------*/
 
-#define tskKERNEL_VERSION_NUMBER       "V10.4.999"
+#define tskKERNEL_VERSION_NUMBER       "V10.4.487#"
 #define tskKERNEL_VERSION_MAJOR        10
 #define tskKERNEL_VERSION_MINOR        4
-#define tskKERNEL_VERSION_BUILD        999
+#define tskKERNEL_VERSION_BUILD        487
 
 /* MPU region parameters passed in ulParameters
  * of MemoryRegion_t struct. */
